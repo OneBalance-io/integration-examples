7af91f6bb2783dce83a34a2c3289e9f58b3ff5cb
el-ll <103944108+el-ll@users.noreply.github.com>
2025-01-24 15:35:58 +0000

    [squash]: deployment fix?

--- turnkey/next.config.ts
+++ turnkey/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/": [
+      "./node_modules/tiny-secp256k1/**/*",
+      "./node_modules/.pnpm/tiny-secp256k1@*/**",
+    ],
+  },
   webpack: (config, options) => {
     config.experiments = {
       asyncWebAssembly: true,
